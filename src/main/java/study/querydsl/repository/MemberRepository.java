097b8d6f65dd90d76dc75f8eafdabb995262e01e
woogie <woogisfree@gmail.com>
2024-03-18 00:15:39 +0900

    Custom Repository 생성 후 테스트

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
 
+import java.util.List;
+
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    //select m from Member m where m.username = ?
+    List<Member> findByUsername(String username);
 }
